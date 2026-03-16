bbaa80a84d76f38ff52c47748246b430acb5083d
shawxavier <xavier.shaw@uon.edu.au>
2026-03-16 22:20:45 +1100

    Merged xinitialise and initialise into one program. Added further comments. Now imports OLED screen too.

--- Week 5/Xavier Test/initialise.py
+++ Week 5/Xavier Test/initialise.py
@@ -1,33 +1,121 @@
+""" An initialisation definition that imports all modules, and returns every sensor used on the robot. This code just
+    has to be called at the start of the program, and it will work.
+
+    Example in other code:
+    from initialise import initialise
+    ir_l, ir_c, ir_r, motor_left, motor_right, ultrasonic, servo, enc, LED, oled = initialise()
+
+    Hopefully that works :)
+"""
+
+
 def initialise():
+    # Bring in a bunch of modules for the robot. As long as the pins are right, this should work...
     from motor import Motor
     from ultrasonic import sonic
     from machine import Pin, PWM, ADC, I2C
-    from time import sleep, perf_counter
-    from rp2 import bootsel_button as boot
+    from time import sleep
     from encoder import Encoder
-    from APDS9960LITE import APDS9960LITE as rgb_sens
-    from ssd1306 import SSD1306_I2C as oled
-    
-    
+    from ssd1306 import SSD1306_I2C
+
+    # Set IR Pin ins as analogue
     ir_l = ADC(Pin(26))
     ir_c = ADC(Pin(27))
     ir_r = ADC(Pin(28))
-    
+
+    # Set motors
     motor_left = Motor("left", 8, 9, 6)
     motor_right = Motor("right", 10, 11, 7)
+
+    # Set ultrasonic sensor trig and echo
+    ultrasonic = sonic(2, 3)
+
+    # Set servo motor pwm pin
+    servo = PWM(Pin(15))
+    servo.freq(50)
+
+    # Set encoder
+    enc = Encoder(18, 19)
+
+    # Set Green LED Indicator
+    LED = Pin("LED", Pin.OUT)
     
-    rgb_i2c = I2C(0, scl=Pin(17) sda=Pin(16))
-    rgb = rgb_sens(rgb_i2c)
-    rgb.prox.enableSensor()
-    
-    ultrasonic = sonic(2,3)
-    
-    servo = PWM(Pin(16))
-    
-    oled_i2c = I2C(0, sda=Pin(12), scl=Pin(13))
-    oled = oled(128, 64, oled_i2c)
-    
-    time.sleeop(0.1)
+    # Initialise Oled Display
+    oled_pin = I2C(0, sda=Pin(4), scl=Pin(5))
+    oled = SSD1306_I2C(128, 64, oled_pin)
+
+    # Wait for a moment for it all to turn on
+    sleep(0.1)
     print("All Initialised!")
+
+    # Send back a list - copy-paste this into the top of the other doc
+    return ir_l, ir_c, ir_r, motor_left, motor_right, ultrasonic, servo, enc, LED, oled
+
+""" Bootwait can be placed into code, and requires the bootsel button to be pressed before the code runs any further
     
-    return ir_l, ir_c, ir_r, motor_left, motor_right, rgb, ultrasonic, servo, oled
+    Example: bootwait()
+"""
+def bootwait():
+    # A command that waits for the button to be pressed before running subsequent code
+    from rp2 import bootsel_button as boot
+    while True:
+        if boot():
+            break
+
+""" angle sets the angle of the servo motor, as a percentage of the duty cycle. You need to specify both the angle and
+    the servo motor.
+    Example: angle(90, servo)
+"""
+def angle(angle, servo):
+    position = int(8000 * ((angle + 7) / 180) + 1000)
+    servo.duty_u16(position)
+
+""" calibrate uses the motors and encoders to proportionally adjust the pwm values (for a set list) and return a dictionary.
+    This assumes that pwm is in a linear relationship to motor speed, which of course it isn't, but is fairly good.
+    Example: pwm = calibrate(motor_left, motor_right, enc)
+    motor_left.duty(pwm[l_50])
+    motor_right.duty(pwm[r_50])
+"""
+def calibrate(left, right, enc, pwm=[30, 50, 80, 100], time=1):
+    from motor import Motor
+    from encoder import Encoder
+    from time import sleep
+
+    values = {}
+
+    left.set_forwards()
+    right.set_forwards()
+
+    for i in pwm:
+        enc.clear_count()
+        left.duty(i)
+        right.duty(i)
+
+        sleep(time)
+
+        l = enc.get_left()
+        r = enc.get_right()
+        print(l)
+        print(r)
+
+        if l > r:
+            l = round((r / l) * i)
+            r = i
+
+        elif r > l:
+            r = round((l / r) * i)
+            l = i
+
+        else:
+            l = i
+            r = i
+
+        values[f"l_{i}"] = l
+        values[f"r_{i}"] = r
+
+        left.duty(0)
+        right.duty(0)
+        sleep(0.5)
+
+    return values
+
