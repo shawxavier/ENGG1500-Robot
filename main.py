099f22b746375d1363345fca753c3d69565b7e50
shawxavier <xavier.shaw@uon.edu.au>
2026-04-13 09:32:24 +1000

    Reinitialised environment to "" Added handling for if diff =+-10 (I thought this would be very rare)

--- main.py
+++ main.py
@@ -176,19 +176,20 @@ while True:
                 motor_left.duty(pwm['l_30'])
                 motor_right.duty(pwm['r_30'])
                 sleep (0.5)
-            elif diff < -10: #move over to the right
+            elif diff <= -10: #move over to the right
                 motor_left.duty(pwm['l_30'])
                 sleep(0.25)
                 motor_left.duty(0)
                 motor_right.duty(pwm['r_30'])
                 sleep(0.25)
-            elif diff > 10:
+            elif diff >= 10:
                 motor_right.duty(pwm['r_30'])
                 sleep(0.25)
                 motor_right.duty(0)
                 motor_left.duty(pwm['l_30'])
             sleep(0.25)
         angle(90, servo)
+    environment = " "
         continue
 
     # Line Position (weighted average)
